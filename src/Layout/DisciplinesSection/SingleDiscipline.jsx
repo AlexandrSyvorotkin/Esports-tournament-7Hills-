ca09ec15e32de0694395d871dcbe06e35e91b0d6
Alex <Alex031994@gmail.com>
2022-04-09 21:18:27 +0300

    singlediscipline component modified

--- src/Layout/DisciplinesSection/SingleDiscipline.jsx
+++ src/Layout/DisciplinesSection/SingleDiscipline.jsx
@@ -1,32 +1,17 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styles from './SingleDiscipline.module.scss'
-import Arrow from '../../Assets/whiteArrow.svg'
-import {motion} from 'framer-motion'
 import {Link} from "react-router-dom";
-import ButtonSecondary from "../../UI/ButtonSecondary";
 
 const SingleDiscipline = ({DisciplineName, img, RoutingPath}) => {
 
-    const [hoverEffect, setHoverEffect] = useState(false)
-
 
     return (
-        <div className={styles.single_discipline}
-             // onMouseEnter={()=> setHoverEffect(true)}
-             // onMouseLeave={()=> setHoverEffect(false)}
+        <Link className={styles.single_discipline}
+              to={RoutingPath}
             >
             <img className={styles.single_discipline_image} src={img} alt=""/>
-            {!hoverEffect ?
-                <Link to={RoutingPath}>{DisciplineName}</Link> :
-                <motion.p
-                    animate={{ y: -20 }}
-                    transition={{ ease: "easeOut", duration: 0.5 }}
-                >ПОДРОБНЕЕ
-                </motion.p>}
-            {/*<div className={styles.arrow}>*/}
-            {/*    <img src={Arrow} alt=""/>*/}
-            {/*</div>*/}
-        </div>
+                <Link to={RoutingPath}>{DisciplineName}</Link>
+        </Link>
     );
 };
 
