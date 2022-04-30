62e6ec94d2cd5638a9ec68178e6e4d99243d13d0
Alex <Alex031994@gmail.com>
2022-04-30 23:53:32 +0300

    modified and prepare to deploy

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -32,9 +32,9 @@ const Menu = ({setMenuActive, menuActive}) => {
                     {/*<li>*/}
                     {/*    <Link to='/positions'><MenuButton>Регламенты</MenuButton></Link>*/}
                     {/*</li>*/}
-                    <li>
-                        <Link to='/tournament-brackets'><MenuButton>Турнирные сетки</MenuButton></Link>
-                    </li>
+                    {/*<li>*/}
+                    {/*    <Link to='/tournament-brackets'><MenuButton>Турнирные сетки</MenuButton></Link>*/}
+                    {/*</li>*/}
                 </ul>
             </div>
         </div>
